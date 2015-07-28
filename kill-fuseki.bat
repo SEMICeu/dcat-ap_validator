995ee70c5e5c5f8a3d3928a124d7264fc152fa3d
emidio <emidiostani@gmail.com>
2015-07-28 16:19:25 +0200

    kill fuseki

--- kill-fuseki.bat
+++ kill-fuseki.bat
@@ -1,1 +1,1 @@
-for /f "tokens=5 delims= " %F in ^(^'netstat -a -o -n^| findstr \"^:3030\"^'^) do taskkill /F /PID %F 
+for /f "tokens=5 delims= " %F in ('netstat -a -o -n^| findstr "3030"') do taskkill /F /PID %F 
