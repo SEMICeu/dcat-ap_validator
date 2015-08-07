f52818dfc90e756a804d9f0bcde7a8dc13eb8b72
emidiostani <Entropia1>
2015-08-07 12:40:00 +0200

    fixed test

--- tests/test-rule-0.js
+++ tests/test-rule-0.js
@@ -1,13 +1,13 @@
 casper.test.begin('test-rule-0', 2, function suite(test) {
 	casper.start(casper.cli.get("url"), function() {
 		this.page.uploadFile('input[type="file"]', '.\\tests-data\\test-rule-0.rdf');
-		this.capture('test-execution/test-rule-0-0.png');
+		this.capture(casper.cli.get("output")+'/test-rule-0-0.png');
 		this.click('button[id="validate"]');
 		});
 
 	casper.then(function() {
 	  this.waitForResource(this.getCurrentUrl(),function() {
-		this.capture('test-execution/test-rule-0-1.png');
+		this.capture(casper.cli.get("output")+'/test-rule-0-1.png');
 		var xml = this.page.content;
 		//this.echo(xml);
 		parser = new DOMParser();
