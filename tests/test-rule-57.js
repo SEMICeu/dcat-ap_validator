caf11f5e860da271150b7386b1f81af9b31dd7bc
emidiostani <Entropia1>
2015-08-07 17:13:58 +0200

    added test-rule-57.js

--- tests/test-rule-57.js
+++ tests/test-rule-57.js
@@ -1,4 +1,4 @@
-var testid = "56", testname = "test-rule-" + testid;
+var testid = "57", testname = "test-rule-" + testid;
 casper.test.begin(testname, 2, function suite(test) {
     casper.start(casper.cli.get("url"), function() {
         var file = '.\\' + casper.cli.get("testdata") + '\\' + testname + '.rdf';
