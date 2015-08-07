39e09c8e1bf6983782d5ae6944fc7e9c6eee8dd6
emidiostani <Entropia1>
2015-08-07 15:25:45 +0200

    added test-rule-1.js

---
+++ tests/test-rule-1.js
@@ -0,0 +1,30 @@
+var testid="1", testname="test-rule-"+testid;
+casper.test.begin(testname, 2, function suite(test) {
+	casper.start(casper.cli.get("url"), function() {
+		var file = '.\\' + casper.cli.get("testdata") + '\\' + testname + '.rdf';
+		this.page.uploadFile('input[type="file"]', file);
+		this.capture(casper.cli.get("output")+'/' + testname + '-0.png');
+		this.click('button[id="validate"]');
+		});
+
+	casper.then(function() {
+	  this.waitForResource(this.getCurrentUrl(),function() {
+		this.capture(casper.cli.get("output")+'/' + testname + '-1.png');
+		var xml = this.page.content;
+		//this.echo(xml);
+		parser = new DOMParser();
+		xmlDoc = parser.parseFromString(xml,'text/xml');
+		var results = xmlDoc.getElementsByTagName("results")[0].childNodes;
+		test.assertEquals(results.length,3);
+		var binding = xmlDoc.getElementsByTagName("binding")[1].textContent.trim();
+		//this.echo(binding);
+		test.assertEquals(binding,testid);
+	  },function() {
+		//page load failed after 5 seconds
+	  },5000);
+	});
+
+casper.run(function() {
+        test.done();
+    });
+});
