295929b426f695d1bad6dd156a03121125da339f
emidiostani <Entropia1>
2015-08-07 12:55:23 +0200

    fix testid

--- tests/test-rule-0.js
+++ tests/test-rule-0.js
@@ -1,14 +1,15 @@
-var test_id='test-rule-0';
-casper.test.begin(test_id, 2, function suite(test) {
+var testid='test-rule-0';
+casper.test.begin(testid, 2, function suite(test) {
 	casper.start(casper.cli.get("url"), function() {
-		this.page.uploadFile('input[type="file"]', '.\\' + casper.cli.get("testdata")+'\\' + test_id + '.rdf');
+		var file = '.\\' + casper.cli.get("testdata") + '\\' + testid + '.rdf';
+		this.page.uploadFile('input[type="file"]', file);
 		this.capture(casper.cli.get("output")+'/' + test_id + '-0.png');
 		this.click('button[id="validate"]');
 		});
 
 	casper.then(function() {
 	  this.waitForResource(this.getCurrentUrl(),function() {
-		this.capture(casper.cli.get("output")+'/' + test_id + '-1.png');
+		this.capture(casper.cli.get("output")+'/' + testid + '-1.png');
 		var xml = this.page.content;
 		//this.echo(xml);
 		parser = new DOMParser();
