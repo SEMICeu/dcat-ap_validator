ecc133c11c6820ba4d2ce2280532a47a82ff5ed4
emidiostani <Entropia1>
2015-08-07 11:55:17 +0200

    added test-exution folder

--- tests/test-rule-0.js
+++ tests/test-rule-0.js
@@ -1,7 +1,7 @@
 casper.test.begin('test-rule-0', 2, function suite(test) {
 	casper.start('http://localhost:3030/dcat-ap_validator.html', function() {
 		this.page.uploadFile('input[type="file"]', 'C:\\Users\\stanie\\dcat-ap_validator\\tests-data\\test-rule-0.rdf');
-		this.capture('rule-0-0.png');
+		this.capture('test-execution/rule-0-0.png');
 		this.click('button[id="validate"]');
 		});
 
@@ -16,7 +16,7 @@ casper.test.begin('test-rule-0', 2, function suite(test) {
 		var binding = xmlDoc.getElementsByTagName("binding")[1].textContent.trim();
 		//this.echo(binding);
 		test.assertEquals(binding,"0");
-		this.capture('rule-0-1.png');
+		this.capture('test-execution/rule-0-1.png');
 	  },function() {
 		//page load failed after 5 seconds
 	  },5000);
