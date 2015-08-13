cb77fe49b50211fc39fdac289f38df6feeaa0ac4
emidiostani <Entropia1>
2015-08-13 10:21:58 +0200

    fixed rule 75

--- tests/test-rule-76.js
+++ tests/test-rule-76.js
@@ -1,4 +1,4 @@
-var testid = "74", testname = casper.cli.get("testname") + testid;
+var testid = "76", testname = casper.cli.get("testname") + testid;
 casper.test.begin(testname, 2, function suite(test) {
     casper.start(casper.cli.get("url") + '/' + casper.cli.get("page"), function() {
         var file = '.\\' + casper.cli.get("testdata") + '\\' + testname + '.rdf';
