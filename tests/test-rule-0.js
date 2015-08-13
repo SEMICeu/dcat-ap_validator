fac3de43cd9415cd9636c5a42063aea7e462fe35
emidiostani <Entropia1>
2015-08-13 12:09:07 +0200

    try 7

--- tests/test-rule-0.js
+++ tests/test-rule-0.js
@@ -1,7 +1,7 @@
 var system = require('system');
 var args = system.args;
 var ab = args[5];
-var myid = ab.split('.')[0].split('-')[2];
+var myid = ab.split('=')[1].split('.')[0].split('-')[2];
 console.log("ab: "+ab);
 console.log("myid: "+myid);
 var testid = myid, testname = casper.cli.get("testname") + testid;
