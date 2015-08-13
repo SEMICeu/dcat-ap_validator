d252c63f7d3fb2671f604d7af991533207dd76fb
emidiostani <Entropia1>
2015-08-13 12:05:14 +0200

    try 6

--- tests/test-rule-0.js
+++ tests/test-rule-0.js
@@ -1,6 +1,6 @@
 var system = require('system');
 var args = system.args;
-var ab = args[4];
+var ab = args[5];
 var myid = ab.split('.')[0].split('-')[2];
 console.log("ab: "+ab);
 console.log("myid: "+myid);
