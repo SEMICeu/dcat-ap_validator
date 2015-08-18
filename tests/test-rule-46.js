cd9db644a013164c91930d92f6e78757204b3d69
emidiostani <Entropia1>
2015-08-18 15:24:18 +0200

    fixed rules 46 and 81

--- tests/test-rule-46.js
+++ tests/test-rule-46.js
@@ -16,7 +16,7 @@ casper.test.begin(testname, 2, function suite(test) {
             parser = new DOMParser();
             xmlDoc = parser.parseFromString(xml, 'text/xml');
             results = xmlDoc.getElementsByTagName("results")[0].childNodes;
-            test.assertEquals(results.length, 15);
+            test.assertEquals(results.length, 13);
             binding = xmlDoc.getElementsByTagName("binding")[1].textContent.trim();
             //this.echo(binding);
             test.assertEquals(binding, testid);
