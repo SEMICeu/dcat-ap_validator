ff3852c8c1a82a8fd3e1b2a51daad2ff14ad818a
emidiostani <Entropia1>
2015-08-26 14:01:01 +0200

    fixed rule 5 and 114

--- tests/test-rule-5.js
+++ tests/test-rule-5.js
@@ -1,5 +1,5 @@
 var testid = "5", testname = casper.cli.get("testname") + testid;
-casper.test.begin(testname, 2, function suite(test) {
+casper.test.begin(testname, 3, function suite(test) {
     casper.start(casper.cli.get("url") + '/' + casper.cli.get("page"), function() {
         var file = '.\\' + casper.cli.get("testdata") + '\\' + testname + '.rdf';
         this.page.uploadFile('input[type="file"]', file);
@@ -14,15 +14,17 @@ casper.test.begin(testname, 2, function suite(test) {
     casper.then(function() {
         this.waitForResource(this.getCurrentUrl(), function() {
             if (casper.cli.has("output")) {this.capture(casper.cli.get("output") + '/' + testname + '-1.png'); }
-            var xml = this.page.content, parser, xmlDoc, results, binding;
+            var xml = this.page.content, parser, xmlDoc, results, binding0, binding1;
             //this.echo(xml);
             parser = new DOMParser();
             xmlDoc = parser.parseFromString(xml, 'text/xml');
             results = xmlDoc.getElementsByTagName("results")[0].childNodes;
             test.assertEquals(results.length, 5);
-            binding = xmlDoc.getElementsByTagName("binding")[1].textContent.trim();
+            binding0 = xmlDoc.getElementsByTagName("result")[0].getElementsByTagName("binding")[1].textContent.trim();
+            test.assertEquals(binding0, testid);
+            binding1 = xmlDoc.getElementsByTagName("result")[1].getElementsByTagName("binding")[1].textContent.trim();
             //this.echo(binding);
-            test.assertEquals(binding, testid);
+            test.assertEquals(binding1, "114");
         }, 3000);
     });
 
