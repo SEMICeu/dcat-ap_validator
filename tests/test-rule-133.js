5ea05f57216af98ac5fbbc1a607119085d43fabb
emidiostani <Entropia1>
2015-08-27 14:45:33 +0200

    added rule 133

--- tests/test-rule-133.js
+++ tests/test-rule-133.js
@@ -1,4 +1,4 @@
-var testid = "131", testname = casper.cli.get("testname") + testid;
+var testid = "133", testname = casper.cli.get("testname") + testid;
 casper.test.begin(testname, 2, function suite(test) {
     casper.start(casper.cli.get("url") + '/' + casper.cli.get("page"), function() {
         var file = '.\\' + casper.cli.get("testdata") + '\\' + testname + '.rdf';
@@ -19,7 +19,7 @@ casper.test.begin(testname, 2, function suite(test) {
             parser = new DOMParser();
             xmlDoc = parser.parseFromString(xml, 'text/xml');
             results = xmlDoc.getElementsByTagName("results")[0].childNodes;
-            test.assertEquals(results.length, 5);
+            test.assertEquals(results.length, 3);
             binding = xmlDoc.getElementsByTagName("binding")[1].textContent.trim();
             //this.echo(binding);
             test.assertEquals(binding, testid);
