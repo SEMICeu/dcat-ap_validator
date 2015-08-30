7a929ab8707fb74914d548464511976aed2b10f8
emidiostani <Entropia1>
2015-08-30 13:26:03 +0200

    removing background

--- pages/js/dcat-ap/dcat-ap_validator-results.js
+++ pages/js/dcat-ap/dcat-ap_validator-results.js
@@ -44,11 +44,10 @@ $(document).ready(function() {
 
     // DataTable, ordering by severity
     table = $('#results').DataTable({"order": [[ 2, "asc" ]], "dom": 'irptflp'});
-    $('table').css({"border": "0px", "padding-top": "10px"});
-    $('thead th').css({"background": "linear-gradient(#eaeaea, white)"});
+    $('table').css({"border": "0px", "padding-top": "10px", "padding-bottom": "10px"});
+    //$('thead th').css({"background": "linear-gradient(#eaeaea, white)"});
     $('thead th:first-child').css({"border-top-left-radius": "10px"});
     $('thead th:last-child').css({"border-top-right-radius": "10px"});
-    $('div.dataTables_length').css({"margin-top": "10px"});
 
     $('tfoot th').each(function () {
         var title, label, input;
