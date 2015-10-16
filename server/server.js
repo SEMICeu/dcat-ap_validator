d7dca0431dd4ac1b7dedfb6b7f743765f6c46f3d
emidiostani <Entropia1>
2015-10-16 11:20:20 +0200

    including server.js in build

--- server/server.js
+++ server/server.js
@@ -3,15 +3,20 @@ var url = require('url');
 var request = require('request');
 var fs = require('fs');
 
-var outputFilename = "session.json";
-var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
-var myhost = "localhost";
-var myport = "3030";
-var baseURL = "http://" + myhost + ":" + myport;
-var sparqlEndpoint = "dcat-ap_validator";
-var defaultEndpoint = baseURL + "/" + sparqlEndpoint;
+var serverport = process.argv[2];      //3000
+var outputFilename = process.argv[3];  // json file containing the graphs created
+var daystodiscard = process.argv[4];   // 1 (1 day) or 0.00348 (5 min)
+var fusekihost = process.argv[5];      // localhost
+var fusekiport = process.argv[6];      // 3030
+var sparqlEndpoint = process.argv[7];  // dcat-ap_validator
 
-http.createServer(onRequest).listen(3000);
+var baseURL = "http://" + fusekihost + ":" + fusekiport;
+var defaultEndpoint = baseURL + "/" + sparqlEndpoint;
+var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+
+http.createServer(onRequest).listen(serverport);
+
+console.log("[LOG] Ready to accept requests...");
 
 function postCode(query, endpoint) {
   // Build the post string from an object
@@ -19,8 +24,8 @@ function postCode(query, endpoint) {
 
   // An object of options to indicate where to post to
   var post_options = {
-      host: myhost,
-      port: myport,
+      host: fusekihost,
+      port: fusekiport,
       path: endpoint + '/update',
       method: 'POST',
       headers: {
@@ -33,7 +38,7 @@ function postCode(query, endpoint) {
   var post_req = http.request(post_options, function(res) {
       res.setEncoding('utf8');
       res.on('data', function (chunk) {
-          console.log('Response: ' + chunk);
+          console.log("[LOG] Data sent");
       });
   });
 
@@ -45,13 +50,12 @@ function postCode(query, endpoint) {
 function removeOldGraphs(jsonContent){
     for(var graph in jsonContent) {
         var value = jsonContent[graph];
-        console.log("key:" + graph + ", value:" + value);
         var now = new Date().getTime();
         var diffDays = (Math.abs((now - value)/(oneDay)));
-        console.log("diff:" + diffDays);
-        if (diffDays > 0.004) {
+        if (diffDays > daystodiscard) {
             delete jsonContent[graph];
             postCode('DROP GRAPH <' + graph + '>', defaultEndpoint); //wipes the named graph in the triple store
+            console.log("[LOG] Dropping graph: " + graph);
         }
     }
 }
@@ -61,36 +65,52 @@ function onRequest(req, res) {
     if (url_parts.pathname == "/getfile") {
         var queryData = url.parse(req.url, true).query;
         if (queryData.url) {
-            console.log("received: " + queryData.url);
+            console.log("[LOG] Serving: " + queryData.url);
             request({
                 url: queryData.url
-            }).on('error', function(e) {
-                res.end(e);
+            }).on('error', function(err) {
+                res.end(err);
+                console.log("[LOG] There was a problem with the request " + queryData.url + "see: " + err);
             }).pipe(res);
             res.setHeader('Access-Control-Allow-Origin', baseURL);
         }
         else {
-            res.end("no url found");
+            res.end("No url found");
         }
     } else if (url_parts.pathname == "/registergraph") {
         var queryData = url.parse(req.url, true).query;
         var graphid = queryData.graphid;
         var creationdate = graphid.substring(graphid.lastIndexOf("/")+1);
        // var creationdate = queryData.creationdate;
-        var jsonContent = JSON.parse(fs.readFileSync(outputFilename));
+        var data;
+        try {
+            data = fs.readFileSync(outputFilename);
+        } catch(err) {
+            if (err.code === 'ENOENT') {
+                console.log("[LOG] File " + outputFilename + " not found!");
+                fs.writeFileSync(outputFilename,"{}");
+                console.log("[LOG] File " + outputFilename + " created");
+                data = fs.readFileSync(outputFilename);
+            } else {
+                console.log("[LOG] There was a problem in reading the file " + outputFilename + "see: " + err);
+                throw err;
+            }
+        }
+        var jsonContent = JSON.parse(data);
         jsonContent[graphid] = creationdate;
+        console.log("[LOG] Adding graph: " + graphid);
         removeOldGraphs(jsonContent);
         fs.writeFile(outputFilename, JSON.stringify(jsonContent, null, 4), function(err) {
             if(err) {
-              console.log(err);
+              console.log("[LOG] There was a problem in writing the file " + outputFilename + "see: " + err);
             } else {
-              console.log("JSON saved to " + outputFilename);
+              console.log("[LOG] JSON saved to " + outputFilename);
             }
         });
         res.setHeader('Access-Control-Allow-Origin', baseURL);
-        res.end("session logged");
+        res.end("Session logged");
     } else {
-        console.log("not a good request");
-        res.end("request not accepted");
+        console.log("[LOG] Not a good request:" + req.url);
+        res.end("Request not accepted");
     }
 }
